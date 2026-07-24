c72b23d029f6684cbdc8ed3eb018f71e6e32bf6f
tommycheung43 <tommycheung43@gmail.com>
2026-07-24 14:53:57 +0800

    add log record in elementary algebra skill

--- elementary_algebra.py
+++ elementary_algebra.py
@@ -7,6 +7,8 @@ import random
 import math
 
 from main import run_script
+import logger_utils
+from logger_utils import setup_agent_logging, write_log
 
 def play_video(url: str) -> str:
     """Opens the student's default web browser to play a tutorial video.
@@ -77,9 +79,9 @@ def algebra():
     return equation, var, float(elementary_algebra_ans)
 
 
-def get_input(message: str) -> str:
-    """Handles getting textual or numerical input from the student via the terminal."""
-    return input(message)
+# def get_input(message: str) -> str:
+#     """Handles getting textual or numerical input from the student via the terminal."""
+#     return input(message)
 
 def algebra_answer(elementary_algebra_ans: float, student_result: float):
     """
@@ -128,7 +130,7 @@ if __name__ == "__main__":
     print(result["messages"][-1].content)
 
     while True:
-        student_q = get_input("\nAsk a question, request a basic review, or type 'ready' to start:")
+        student_q = input("\nAsk a question, request a basic review, or type 'ready' to start:")
 
         if student_q.lower().strip() in ["yes","no question", "ready", "start", "none","nope","no questions","i'm ready","quiz","let's start"]:
             print("\nGreat! Let's move on to the quiz phase.")
@@ -162,7 +164,7 @@ if __name__ == "__main__":
 
     while True:
         
-        ans_str = get_input(f"\nWhat is the value of {var}? (or ask a question / request an example) ").strip()
+        ans_str = input(f"\nWhat is the value of {var}? (or ask a question / request an example) ").strip()
 
         if ans_str.lower() in ["quit", "exit"]:
             print("Exiting quiz...")
@@ -229,7 +231,7 @@ if __name__ == "__main__":
             print("1. Keep practicing another question")
             print("2. Move to another topic ")
             print("==================================================")
-            user_choice = get_input("Please enter option (1 or 2): ")
+            user_choice = input("Please enter option (1 or 2): ")
 
             if user_choice.strip() == "2":
                 import os
