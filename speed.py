c1cc945c0a0ba8f63846e486f86087391bf25d94
tommycheung43 <tommycheung43@gmail.com>
2026-07-24 11:30:29 +0800

    add log record in speed skill

--- speed.py
+++ speed.py
@@ -9,6 +9,9 @@ import math
 from main import run_script
 from main import menu_mapping
 
+import logger_utils
+from logger_utils import setup_agent_logging, write_log
+
 def play_video(url: str) -> str:
     """Opens the student's default web browser to play a tutorial video.
     
@@ -79,9 +82,9 @@ def speed_question_text(agent, speed_dict: dict):
     return result["messages"][-1].content
 
 
-def get_input(message: str) -> str:
-    """Handles getting textual or numerical input from the student via the terminal."""
-    return input(message)
+# def get_input(message: str) -> str:
+#     """Handles getting textual or numerical input from the student via the terminal."""
+#     return input(message)
 
 def speed_check_answer(speed_correct_answer: float, student_result: float):
     """
@@ -98,7 +101,7 @@ if __name__ == "__main__":
     agent = create_deep_agent(
         model="ollama:gemma4:cloud",  
         backend=backend,
-        tools=[play_video,run_script],
+        tools=[play_video,run_script,write_log],
         skills=[str(Path(root_dir) / "skills")],
         interrupt_on={
             "write_file": True,
@@ -107,6 +110,8 @@ if __name__ == "__main__":
         },
         checkpointer=checkpointer,
     )
+
+    setup_agent_logging(agent)
 
     message = (
         f"1. Please introduce the concept of Speed, Distance, and Time following the speed-docs skill.\n"
@@ -129,7 +134,7 @@ if __name__ == "__main__":
     print(result["messages"][-1].content)
 
     while True:
-        student_q = get_input("\nAsk a question, request a basic review, or type 'ready' to start:")
+        student_q = input("\nAsk a question, request a basic review, or type 'ready' to start:")
 
         if student_q.lower().strip() in ["yes","no question", "ready", "start", "none","nope","no questions","i'm ready","quiz","let's start"]:
             print("\nGreat! Let's move on to the quiz phase.")
@@ -167,7 +172,7 @@ if __name__ == "__main__":
 
     while True:
         
-        ans_str = get_input(f"\nWhat is the answer? (or ask a question / request an example) ").strip()
+        ans_str = input(f"\nWhat is the answer? (or ask a question / request an example) ").strip()
 
         if ans_str.lower() in ["quit", "exit"]:
             print("Exiting quiz...")
@@ -236,7 +241,7 @@ if __name__ == "__main__":
             print("1. Keep practicing another question")
             print("2. Move to another topic ")
             print("==================================================")
-            user_choice = get_input("Please enter option (1 or 2): ")
+            user_choice = input("Please enter option (1 or 2): ")
 
             if user_choice.strip() == "2":
                 import os
